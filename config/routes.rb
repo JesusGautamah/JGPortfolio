f3f0aaa7d41669ee65881fc24d229cf4f3bbeccc
JesusGautamah <lima.jesuscc@gmail.com>
2021-09-30 11:32:30 -0300

    some style and changes

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,15 @@ Rails.application.routes.draw do
   resources :projects
   resources :sections, only: [:index, :show]
 
+  resources :projects do 
+    resources :git_repositories
+    resources :videos
+    resources :articles
+    resources :docs
+    resources :projects
+  end
+
+
   get    'sign_in'   => 'sessions#new'
   post   'sign_in'   => 'sessions#create'
   get 'sign_out'  => 'sessions#destroy'
